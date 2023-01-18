6c5ced5066de61c351e312b26ea85d53165ac640
SawickiMike <120393520+SawickiMike@users.noreply.github.com>
2023-01-18 18:04:05 +0100

    Refactor to common base interface. closes #11

--- pet-clinic-data/src/main/java/com/sawicki/mypetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/sawicki/mypetclinic/services/VetService.java
@@ -2,10 +2,6 @@ package com.sawicki.mypetclinic.services;
 
 import com.sawicki.mypetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService <Vet, Long> {
 
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
 }
